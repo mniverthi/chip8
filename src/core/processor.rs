8021839520a6363c41057c60bacc71ecf707047d
Manoj Niverthi <manojniverthi@gmail.com>
2023-06-19 18:45:10 -0400

    added test config and first few tests

--- src/core/processor.rs
+++ src/core/processor.rs
@@ -32,6 +32,7 @@ impl Processor {
         keyboard_buffer_: ram::KeyboardBuffer,
     ) -> Self {
         Processor {
+            pc: 0x200,
             ram: Rc::new(ram_),
             display_buffer: Rc::new(display_ram_),
             keyboard_buffer: Rc::new(keyboard_buffer_),
@@ -39,7 +40,7 @@ impl Processor {
             ..Default::default()
         }
     }
-    pub fn init_ram(&mut self, rom: &rom::Rom, fonts: &[u8]) -> Result<(), &str> {
+    pub fn init_ram(&mut self, rom: &rom::Rom, fonts: &[u8]) -> Result<(), &'static str> {
         let _ = match Rc::get_mut(&mut self.ram) {
             Some(s) => {
                 s.buffer[0..consts::FONT_SET_SIZE].clone_from_slice(fonts);
@@ -52,14 +53,14 @@ impl Processor {
     }
     pub fn cycle(&mut self) -> Option<CycleStatus> {
         let instr_nibbles = utils::nibble_split(
-            &(self.ram.as_ref().buffer
+            &(self.ram.buffer
                 [(self.pc) as usize..((self.pc + (consts::OP_CODE_BYTES as u16)) as usize)]),
         );
         self.pc += consts::OP_CODE_BYTES as u16;
         let (opcode, x, y, n) = instr_nibbles;
         let nn = (y << 4) | n;
         let nnn = ((x as u16) << 8) | ((y as u16) << 4) | (n as u16);
-        let keyboard = self.keyboard_buffer.as_ref().buffer.clone();
+        let keyboard = self.keyboard_buffer.buffer.clone();
 
         match (opcode, x, y, n) {
             // Clears screen
@@ -76,7 +77,7 @@ impl Processor {
             (0xD, _, _, _) => {
                 let x_coord = self.registers[x as usize] % (consts::CHIP8_WIDTH as u8);
                 let y_coord = self.registers[y as usize] % (consts::CHIP8_HEIGHT as u8);
-                let sprite_vals = &self.ram.as_ref().buffer
+                let sprite_vals = &self.ram.buffer
                     [(self.idx_register as usize)..((self.idx_register + (n as u16)) as usize)];
                 let vram = &mut Rc::get_mut(&mut self.display_buffer)?.buffer;
                 for i in 0..n {
@@ -290,7 +291,7 @@ impl Processor {
                 }
             }
             (0xF, _, 6, 5) => {
-                let ram_ref = self.ram.as_ref().buffer;
+                let ram_ref = self.ram.buffer;
                 for i in 0..(x + 1) {
                     self.registers[i as usize] = ram_ref[(self.idx_register + i as u16) as usize];
                 }
@@ -308,5 +309,154 @@ impl Processor {
             }
         }
         Some(CycleStatus::Continue)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::consts;
+    use crate::processor::Processor;
+    use crate::{ram, rom};
+    use std::rc::Rc;
+
+    const START_PC: u16 = 0xF00;
+    const NEXT_PC: u16 = START_PC + (consts::OP_CODE_BYTES as u16);
+    const SKIPPED_PC: u16 = START_PC + ((2 * consts::OP_CODE_BYTES) as u16);
+
+    fn update_buffer(buffer: &mut [u8], address: usize, value: u8) {
+        buffer[address] = value;
+    }
+
+    fn build_processor() -> Result<Processor, &'static str> {
+        let ram_ = ram::Ram {
+            ..Default::default()
+        };
+
+        let display_ram_ = ram::DisplayBuffer {
+            ..Default::default()
+        };
+
+        let keyboard_buffer_ = ram::KeyboardBuffer {
+            ..Default::default()
+        };
+        let mut proc = Processor::new(ram_, display_ram_, keyboard_buffer_);
+        proc.pc = START_PC;
+        proc.registers = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+
+        Ok(proc)
+    }
+
+    #[test]
+    fn test_initial_state() -> Result<(), &'static str> {
+        let ram_ = ram::Ram {
+            ..Default::default()
+        };
+
+        let display_ram_ = ram::DisplayBuffer {
+            ..Default::default()
+        };
+
+        let keyboard_buffer_ = ram::KeyboardBuffer {
+            ..Default::default()
+        };
+        let mut proc = Processor::new(ram_, display_ram_, keyboard_buffer_);
+        assert_eq!(proc.pc, 0x200);
+        assert_eq!(proc.stack_pointer, 0);
+        assert_eq!(proc.stack, [0; 16]);
+
+        proc.init_ram(&rom::Rom::default(), &consts::FONT_SET)?;
+
+        // First char in font: 0
+        assert_eq!(proc.ram.buffer[0..5], [0xF0, 0x90, 0x90, 0x90, 0xF0]);
+        // Last char in font: F
+        assert_eq!(
+            proc.ram.buffer[consts::FONT_SET.len() - 5..consts::FONT_SET.len()],
+            [0xF0, 0x80, 0xF0, 0x80, 0x80]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_opcode_00e0() -> Result<(), &'static str> {
+        let mut processor = build_processor()?;
+
+        let display_ram = match Rc::get_mut(&mut processor.display_buffer) {
+            Some(t) => &mut t.buffer,
+            None => return Err("Failed test, could not retrieve display buffer"),
+        };
+
+        let ram = match Rc::get_mut(&mut processor.ram) {
+            Some(t) => &mut t.buffer,
+            None => return Err("Failed test, could not retrieve ram buffer"),
+        };
+
+        *display_ram = [[128; consts::CHIP8_WIDTH]; consts::CHIP8_HEIGHT];
+        update_buffer(ram, (START_PC + 1) as usize, 0xE0);
+
+        processor.cycle();
+
+        for y in 0..consts::CHIP8_HEIGHT {
+            for x in 0..consts::CHIP8_WIDTH {
+                assert_eq!(processor.display_buffer.buffer[y][x], 0);
+            }
+        }
+        assert_eq!(processor.pc, NEXT_PC);
+        Ok(())
+    }
+
+    #[test]
+    fn test_opcode_00ee() -> Result<(), &'static str> {
+        let mut processor = build_processor()?;
+
+        let ram = match Rc::get_mut(&mut processor.ram) {
+            Some(t) => &mut t.buffer,
+            None => return Err("Failed test, could not retrieve ram buffer"),
+        };
+
+        update_buffer(ram, (START_PC + 1) as usize, 0xEE);
+
+        processor.stack_pointer = 5;
+        processor.stack[4] = 0x6666;
+
+        processor.cycle();
+
+        assert_eq!(processor.stack_pointer, 4);
+        assert_eq!(processor.pc, 0x6666);
+        Ok(())
+    }
+
+    #[test]
+    fn test_opcode_1nnn() -> Result<(), &'static str> {
+        let mut processor = build_processor()?;
+
+        let ram = match Rc::get_mut(&mut processor.ram) {
+            Some(t) => &mut t.buffer,
+            None => return Err("Failed test, could not retrieve ram buffer"),
+        };
+
+        update_buffer(ram, (START_PC) as usize, 0x16);
+        update_buffer(ram, (START_PC + 1) as usize, 0x66);
+        processor.cycle();
+        assert_eq!(processor.pc, 0x0666);
+        assert_eq!(processor.stack_pointer, 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_opcode_2nnn() -> Result<(), &'static str> {
+        let mut processor = build_processor()?;
+
+        let ram = match Rc::get_mut(&mut processor.ram) {
+            Some(t) => &mut t.buffer,
+            None => return Err("Failed test, could not retrieve ram buffer"),
+        };
+
+        update_buffer(ram, (START_PC) as usize, 0x26);
+        update_buffer(ram, (START_PC + 1) as usize, 0x66);
+        processor.cycle();
+        assert_eq!(processor.pc, 0x0666);
+        assert_eq!(processor.stack_pointer, 1);
+        assert_eq!(processor.stack[0], NEXT_PC);
+        Ok(())
     }
 }
